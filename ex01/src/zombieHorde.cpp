1a79caa30185bc3c9db3f6eace093cf52caa3a13
stanislas1200 <34397853+stanislas1200@users.noreply.github.com>
2023-09-17 13:15:41 +0200

    ex02

--- ex01/src/zombieHorde.cpp
+++ ex01/src/zombieHorde.cpp
@@ -3,6 +3,8 @@
 Zombie* zombieHorde( int N, std::string name )
 {
 	Zombie* horde = new Zombie[N];
+    if (!horde)
+        return (NULL);
 
 	for (int i = 0; i < N; i++)
 		horde[i].setName(name);
